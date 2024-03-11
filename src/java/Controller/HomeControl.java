b988d8393290c603e423cbd134c5578c7d13c279
Tiến_Đạt <Tin_t@BastardFromHell>
2024-03-11 21:28:55 +0700

    flip the homecontrol to make spaces for a filter form later

--- src/java/Controller/HomeControl.java
+++ src/java/Controller/HomeControl.java
@@ -15,37 +15,34 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public class HomeControl extends HttpServlet {
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ProductDAO dao = new ProductDAO();
+        int minPrice = Integer.parseInt(request.getParameter("minPrice"));
+        int maxPrice = Integer.parseInt(request.getParameter("maxPrice"));
+        int category = Integer.parseInt(request.getParameter("category"));
+        int language = Integer.parseInt(request.getParameter("language"));
         
+        List<Product> filtered = dao.myFilter(minPrice, maxPrice, language, category);
+        request.setAttribute("filtered", filtered);
+        doGet(request, response);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProductDAO dao = new ProductDAO();
         List<Subcategory> subcateList = dao.getallSubCategory();
         request.setAttribute("subcateList", subcateList);
-
         List<Category> cateList = dao.getallCategory();
         request.setAttribute("cateList", cateList);
-
         List<Games> gameList = dao.getallGames();
         request.setAttribute("gameList", gameList);
-
         List<Language> langList = dao.getallLanguage();
         request.setAttribute("langList", langList);
-
+        
         List<Product> productList = dao.getallProduct();
         request.setAttribute("productList", productList);
-
         request.getRequestDispatcher("/indexes/index-logined.jsp").forward(request, response);
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
     }
 }
